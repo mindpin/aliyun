0030a55a4bdec759dee2cb910d6428806da9c417
fushang318 <fushang318@gmail.com>
2014-07-25 10:47:11 +0800

    init

---
+++ lib/aliyun.rb
@@ -0,0 +1,18 @@
+require 'openssl'
+require 'base64'
+require 'digest/md5'
+require 'net/http'
+
+require "aliyun/version"
+
+require 'aliyun/error'
+require 'aliyun/parser'
+require 'aliyun/signature'
+require 'aliyun/connection'
+require 'aliyun/object'
+require 'aliyun/bucket'
+require 'aliyun/service'
+
+
+module Aliyun
+end
---
+++ lib/aliyun/signature.rb
@@ -0,0 +1,47 @@
+module Aliyun
+  class Signature
+    
+    def self.generate(access_key_id, secret_access_key, options)
+      method = options.delete(:method)
+      path   = options.delete(:path)
+      headers = options.delete(:headers) || {}
+      md5 = headers.delete :md5
+      content_type = headers.delete :content_type
+
+      date = Time.now.gmtime.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+      oss_sign = ali_oss_sign(secret_access_key, method, date, path,
+        :headers => headers, :md5 => md5, :content_type => content_type)
+
+      hash = {'Authorization' => "OSS #{access_key_id}:#{oss_sign}", 'Date' => date}
+
+      hash.merge!(headers) if !headers.empty?
+      # 上传文件不传 md5 也行
+      # hash.merge!("Content-MD5" => md5) if !md5.nil? && !md5.empty?
+      hash.merge!("Content-Type" => content_type) if !content_type.nil? && !content_type.empty?
+
+      hash
+    end
+
+    private
+    def self.ali_oss_sign(secret_access_key, verb, date, res,options={})
+      digest  = OpenSSL::Digest::Digest.new('sha1')
+
+      md5 = options[:md5] || ''
+      ty = options[:content_type] || ''
+
+      header = ''
+      unless options[:headers].empty?
+        headers = options[:headers]
+        header = headers.keys.sort.map{|keya|"#{keya}:#{headers[keya]}"}*"\n" + "\n"
+      end
+
+      str = "#{verb}\n\n#{ty}\n#{date}\n#{header}#{res}"
+      
+      bytemac = OpenSSL::HMAC.digest(digest, secret_access_key, str)
+      res = Base64.encode64(bytemac).strip
+    end
+
+  end
+end
+
